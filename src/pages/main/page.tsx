034bad1c5ef22c1d5df36bce235506e28c71342f
nozesp <nozerospace@gmail.com>
2025-07-19 13:37:00 +0500

    contact-form

--- src/pages/main/page.tsx
+++ src/pages/main/page.tsx
@@ -1,11 +1,9 @@
-import { ContactForm } from "src/feature/intex";
 
 export const MainPage = () => {
   return (
     <div>
       <h1>Main Pag</h1>
       <p>Welcome to the main page!</p>
-      <ContactForm></ContactForm>
     </div>
   );
 };
